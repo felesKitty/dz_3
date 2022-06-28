23a7ffd17c84bb4557d7d36d3504608906dbc9a6
felesKitty <feleskitty1@gmail.com>
2022-06-28 08:06:57 +0500

    dz3.

--- dz3_6_7.py
+++ dz3_6_7.py
@@ -1,13 +1,13 @@
 
 def int_func():
-     for words in input('If you want to see a miracle, '
+     for i_words in input('If you want to see a miracle, '
                         'enter any words, which contains'
                         'lower latin leters - \n').split():
          buk = 0
-         for buk2 in words:
+         for buk2 in i_words:
              if 97 <= ord(buk2) <= 122:
                 buk += 1
-         print(words.title() if buk == len(words)
+         print(i_words.title() if buk == len(i_words)
               else f'{words} - Error! Not this symbols!')
 
 int_func()
